4988548ff2bdbb218dc2c74c18524d17df0bcd9f
Tim <timdobranski@gmail.com>
2023-01-18 20:57:19 -0800

    Wed night pair progress

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,6 +1,22 @@
 var models = require('../models');
+var mysql = require('mysql2');
+var {connection: connection} = require('../db');
+
 
 module.exports = {
-  get: function (req, res) {}, // a function which handles a get request for all messages
-  post: function (req, res) {} // a function which handles posting a message to the database
+  get: function (req, res) {
+    // Querying the database
+    //  SELECT * FROM messages;
+    // Inner join users id and rooms id > to get usernames and room names
+    // Returning the results with res.send()
+    connection.query(
+      'SELECT * FROM messages', function(error, results, fields) {
+        res.send(results);
+      }
+    );
+
+  }, // a function which handles a get request for all messages
+  post: function (req, res) {
+
+  } // a function which handles posting a message to the database
 };
--- server/db/index.js
+++ server/db/index.js
@@ -9,4 +9,12 @@ var mysql = require('mysql2');
 // OR
 // user: 'root', password: 'some_password_you_created_at_install'
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'chat'
+});
+
+module.exports.connection = connection;
+
 
